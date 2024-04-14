5a5c46cc3f8e395c87e483b939e5ebbe0c1be436
Mayank singh <mayank23378@gmail.com>
2024-04-14 19:02:12 +0530

    learn to work on route

--- project.js
+++ project.js
@@ -8,11 +8,20 @@ app.get("/users",  (req,res) => {
    return res.json(users);
 });
 
-app.get("/users/:id",(req,res)=> {
+app
+.route("/users/:id")
+.get((req,res)=> {
    const id = Number(req.params.id);
    const user = users.find( (user) => user.id === id );
    return res.json(user)
-});
+})
+.patch( (req,res) => {
+   return  res.json( { status : "panding"});
+})
+.delete( (req,res) => {
+   return res.json( { status : "panding"});
+})
+
 
 app.listen(port,  () => {
    console.log(`server stated at port no : ${port}`);
