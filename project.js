ad00e7ebbfd1a7ac26c5a5971731cb51e7375ae5
Mayank singh <mayank23378@gmail.com>
2024-04-14 23:44:58 +0530

    learn to use post to send data using postman

--- project.js
+++ project.js
@@ -2,10 +2,17 @@ const express = require("express");
 const users = require("./MOCK_DATA.json")
 const app = express()
 const port = 3000;
+//middileware - plugin
+app.use(express.urlencoded({ extended: false }));
 
-
+//routs
 app.get("/users",  (req,res) => {
    return res.json(users);
+});
+app.post("/users",  (req,res) => {
+   const body = req.body;
+   console.log("body", body);
+   return res.json({ status: " it's panding"});
 });
 
 app
@@ -20,7 +27,7 @@ app
 })
 .delete( (req,res) => {
    return res.json( { status : "panding"});
-})
+});
 
 
 app.listen(port,  () => {
