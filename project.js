d33b76e00b0ec84282a93d17dc1e3bb05f2af72e
Mayank singh <mayank23378@gmail.com>
2024-04-17 13:57:30 +0530

    learn to connect mongodb

--- project.js
+++ project.js
@@ -1,8 +1,41 @@
 const express = require("express");
-const users = require("./MOCK_DATA.json");
 const app = express();
 const port = 3000;
+
+const users = require("./MOCK_DATA.json");
 const fs = require('fs');
+
+const mongoose = require("mongoose");
+
+//connecting mongodb 
+mongoose
+.connect("mongodb://localhost:27017/my-website")
+.then(()=> console.log(`mongodb connected`))
+.catch((err)=> console.log("error occur",err))
+
+//schema for mongo db
+const userschema = new mongoose.Schema({      //.schema is biltin function
+   firstName: {
+      type: String,
+      required: true,
+   },
+   lastName: {
+      type: String,
+   },
+   email: {
+      type: String,
+      required: true,
+      unique: true,
+   },
+   gender: {
+      type: String,
+   },
+   jobTitle: {
+      type: String,
+   }
+});
+const myuser = mongoose.model("users",userschema)
+
 
 //middileware - plugin
 app.use(express.urlencoded({ extended: false }));
@@ -11,11 +44,15 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use((req,res, next) => {
    fs.appendFile("log.txt",`\n${Date.now()} : ${req.ip} : ${req.method} : ${req.path}`, (err,data) => {
+      console.log(`its a middilware 2`);
       next()
    });
 });
 
 //routs
+app.get("/",(req,res)=> {
+   return res.json("its a home page")
+})
 app.get("/users",  (req,res) => {
    return res.json(users);
 });
